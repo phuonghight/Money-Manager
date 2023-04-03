031e95ce4e41809c3d6911c3b848cb7a3464cab7
hoangphuong <hoangphuong270703@gmail.com>
2023-04-04 00:13:43 +0700

    footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,11 @@
+<script setup>
+import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header.vue';
+</script>
+
 <template>
+  <Header />
   <slot />
+
+  <Footer />
 </template>
