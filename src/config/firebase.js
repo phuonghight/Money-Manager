a8fbe5f81bd7e071952299d6b3c75cd8446daed3
hoangphuong <hoangphuong270703@gmail.com>
2023-04-04 23:56:00 +0700

    env variable

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,17 +4,17 @@ import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyD6nFTRJV1152Z4il1qCmgTtygCmebd8Aw',
-  authDomain: 'imoney-5dfc8.firebaseapp.com',
-  projectId: 'imoney-5dfc8',
-  storageBucket: 'imoney-5dfc8.appspot.com',
-  messagingSenderId: '801495106591',
-  appId: '1:801495106591:web:9cef89ce4929d8dea8412a',
-  measurementId: 'G-N297LLFL0G'
+  apiKey: import.meta.env.VITE_fIREBASE_CONFIG_API_KEY,
+  authDomain: import.meta.env.VITE_fIREBASE_CONFIG_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_fIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_fIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_fIREBASE_CONFIG__MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_fIREBASE_CONFIG_APP_ID,
+  measurementId: import.meta.env.VITE_fIREBASE_CONFIG_MEASUREMNT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const firestore = getFirestore();
+const firestore = getFirestore(app);
 
-export { app, auth, firestore };
+export { auth, firestore };
